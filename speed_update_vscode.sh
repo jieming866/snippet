bcb1720449e7cc925fd35300a55d7b066974a5d7
jieming866 <songjieming@outlook.com>
2022-09-15 23:31:37 +0800

    remove curl stderr redirec

--- speed_update_vscode.sh
+++ speed_update_vscode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-location=$(curl --silent --output /dev/null --write-out '%{redirect_url}' 'https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64' 2>/dev/null)
+location=$(curl --silent --output /dev/null --write-out '%{redirect_url}' 'https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64')
 if [ -z "$location" ]
 then
     echo 'Fialed to get vscode location!'
